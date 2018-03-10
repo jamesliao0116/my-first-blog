d16f40f2a93fc7bbe9800badb8acce57b64bfa95
jamesliao116 <jamesliao0116@gmail.com>
2018-03-10 20:35:20 +1100

    add photo for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,13 +17,13 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib import staticfiles
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^static/(?P<path>.*)', 'django.views.static.serve',
+        {'document_root': '/Users/ya25854/django1/mysite/blog/static/img'}),
 ]
 
-urlpatterns += staticfiles_urlpatterns
